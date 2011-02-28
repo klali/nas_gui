4b3086ba59dd3c768305f32b180d2facd5a76e3d
Klas Lindfors <klali@avm.se>
2011-02-28 20:59:24 +0100

    correct application name

--- config/environments/production-real.rb
+++ config/environments/production-real.rb
@@ -1,4 +1,4 @@
-Rails3SubdomainDevise::Application.configure do
+NasGui::Application.configure do
   # Settings specified here will take precedence over those in config/environment.rb
 
   # The production environment is meant for finished, "live" apps.
