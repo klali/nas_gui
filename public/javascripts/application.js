13304862dbbd7847c2cee258e6dba1cb38c36a28
Klas Lindfors <klali@avm.se>
2011-03-25 16:09:27 +0100

    change order of javascript, use settings for facebox for images

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+$.facebox.settings.closeImage = '/images/closelabel.png';
+$.facebox.settings.loadingImage = '/images/loading.gif';
+
 $(document).ready(function($) {
-    $('a[rel*=facebox]').facebox({ loadingImage : '/images/loading.gif', closeImage : '/images/closelabel.png' })
+    $('a[rel*=facebox]').facebox();
     $('ul.sortable').nestedSortable({
       listType: 'ul',
       disableNesting: 'no-nest',
